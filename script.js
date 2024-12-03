1c6f3545034e39d6088b859381d3c62ab7466176
Raiden R. (Rafael) Kaslana <67449286+RaidenRafael@users.noreply.github.com>
2024-12-03 16:16:03 +0800

    FirstUpd

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to show the Sign-Up section
+function showSignUp() {
+    document.getElementById("login-section").style.display = "none";
+    document.getElementById("signup-section").style.display = "block";
+}
+
+// Function to show the Login section
+function showLogin() {
+    document.getElementById("signup-section").style.display = "none";
+    document.getElementById("login-section").style.display = "block";
+}
